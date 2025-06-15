612dda9a4b105a9ff18714c9ee9d6c087e831966
simkus972 <modestas.simkus11@gmail.com>
2025-06-15 14:31:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply('TharZarBot aktyvuotas 🌟'));
+bot.command('ping', (ctx) => ctx.reply('pong'));
+
+bot.launch();
